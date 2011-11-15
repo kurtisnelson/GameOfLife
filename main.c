4428ccde2ac6f66d6af79cc7710f18cfa70c24ba
Kurt Nelson <kurtisnelson@gmail.com>
2011-11-15 00:00:41 -0500

    Fully implemented

--- main.c
+++ main.c
@@ -10,7 +10,9 @@
 #include "lib/input.h"
 #include "images.h"
 State state = RESET;
-Cell gameArr[GAME_W * GAME_H];
+View vMode = NORMAL;
+Grid grid = CUSTOM;
+static Cell gameArr[GAME_W * GAME_H];
 int
 main()
 {
@@ -22,14 +24,16 @@ main()
         reset();
       if (state == TITLE)
         titleScreen();
-      if (state == NORMAL)
-        normal();
+      if (state == EDIT)
+        edit();
+      if (state == RUN || state == RUN1)
+        run();
     }
   return (0);
 }
 
 void
-normal()
+edit()
 {
   int r = GAME_H / 2;
   int c = GAME_W / 2;
@@ -43,13 +47,14 @@ normal()
       if (KEY_DOWN_NOW(KEY_A))
         {
           KEY_RELEASED_WAIT(KEY_A);
-          if (GET_CELL(r,c) == ALIVE)
+          if (GET_CELL(r,c) == ALIVE || GET_CELL(r,c) == BIRTH)
             GET_CELL(r,c) = DEAD;
-          else if (GET_CELL(r,c) == DEAD)
+          else if (GET_CELL(r,c) == DEAD || GET_CELL(r,c) == DIEING)
             GET_CELL(r,c) = ALIVE;
           moveCrosshairs(r, c, r, c);
           flipPage();
           moveCrosshairs(r, c, r, c);
+          grid = CUSTOM;
         }
       if (KEY_DOWN_NOW(KEY_LEFT))
         {
@@ -95,6 +100,61 @@ normal()
           moveCrosshairs(r, c, newR, c);
           r = newR;
         }
+      if (KEY_DOWN_NOW(KEY_L))
+        {
+          KEY_RELEASED_WAIT(KEY_L);
+          if (vMode == NORMAL)
+            vMode = NEXT;
+          else
+            vMode = NORMAL;
+          drawGrid();
+          flipPage();
+          drawGrid();
+        }
+      //Swap in an example game.
+      if (KEY_DOWN_NOW(KEY_R))
+        {
+          KEY_RELEASED_WAIT(KEY_R);
+          if (grid != PRESET1)
+            {
+              grid = PRESET1;
+              for (int i = 0; i < GAME_W * GAME_H; i++)
+                gameArr[i] = DEAD;
+              gameArr[0] = ALIVE;
+              gameArr[1] = ALIVE;
+              gameArr[2] = ALIVE;
+              drawGrid();
+              flipPage();
+              drawGrid();
+            }
+          else if (grid != PRESET2)
+            {
+              grid = PRESET2;
+              for (int i = 0; i < GAME_W * GAME_H; i++)
+                gameArr[i] = DEAD;
+              gameArr[1] = ALIVE;
+              gameArr[GAME_W+2] = ALIVE;
+              gameArr[2*GAME_W] = ALIVE;
+              gameArr[2*GAME_W+1] = ALIVE;
+              gameArr[2*GAME_W+2] = ALIVE;
+              drawGrid();
+              flipPage();
+              drawGrid();
+            }
+        }
+      //Run the simulation
+      if (KEY_DOWN_NOW(KEY_START))
+        {
+          KEY_RELEASED_WAIT(KEY_START);
+          state = RUN;
+          return;
+        }
+      if (KEY_DOWN_NOW(KEY_B))
+        {
+          KEY_RELEASED_WAIT(KEY_B);
+          state = RUN1;
+          return;
+        }
       if (KEY_DOWN_NOW(KEY_SELECT))
         {
           KEY_RELEASED_WAIT(KEY_SELECT);
@@ -119,7 +179,7 @@ titleScreen()
     {
       if (KEY_DOWN_NOW(KEY_START))
         {
-          state = NORMAL;
+          state = EDIT;
           return;
         }
       if (KEY_DOWN_NOW(KEY_SELECT))
@@ -144,6 +204,118 @@ drawGrid()
 }
 
 void
+run()
+{
+  volatile u32 slow = 0;
+  int count = 0;
+  while (1)
+    {
+      if(vMode == NEXT && count%2)
+        applyVerbs();
+      else
+        timeStep();
+      drawGrid();
+      flipPage();
+      if(state == RUN1)
+        {
+          state = EDIT;
+          return;
+        }
+      while (slow < 50000)
+        {
+          if (KEY_DOWN_NOW(KEY_START))
+            {
+              state = EDIT;
+              return;
+            }
+          if (KEY_DOWN_NOW(KEY_SELECT))
+            {
+              KEY_RELEASED_WAIT(KEY_SELECT);
+              state = RESET;
+              return;
+            }
+          if (KEY_DOWN_NOW(KEY_L))
+            {
+              KEY_RELEASED_WAIT(KEY_L);
+              if (vMode == NORMAL)
+                vMode = NEXT;
+              else
+                vMode = NORMAL;
+            }
+          slow++;
+        }
+      slow = 0;
+      count++;
+    }
+}
+
+void
+timeStep()
+{
+  Cell newGameArr[GAME_W * GAME_H];
+  for (int i = 0; i < GAME_W * GAME_H; i++)
+    {
+      int c = neighborCount(i / GAME_W, i % GAME_W);
+      if (c < 2 && gameArr[i] == ALIVE)
+        newGameArr[i] = (vMode==NORMAL?DEAD:DIEING);
+      else if (c > 3 && gameArr[i] == ALIVE)
+        newGameArr[i] = (vMode==NORMAL?DEAD:DIEING);
+      else if (c == 3 && gameArr[i] == DEAD)
+        newGameArr[i] = (vMode==NORMAL?ALIVE:BIRTH);
+      else
+        newGameArr[i] = gameArr[i];
+    }
+  for (int i = 0; i < GAME_W * GAME_H; i++)
+    {
+      gameArr[i] = newGameArr[i];
+    }
+}
+
+//Change Cells to ALIVE or DEAD if dieing or birthing
+void
+applyVerbs()
+{
+  for(int i = 0; i<GAME_W*GAME_H; i++)
+          {
+            if(gameArr[i] == BIRTH)
+              gameArr[i] = ALIVE;
+            else if(gameArr[i] == DIEING)
+              gameArr[i] = DEAD;
+          }
+}
+
+int
+neighborCount(int r, int c)
+{
+  int count = 0;
+  //Left
+  if (gameArr[CLAMP_C(c - 1) + (r * GAME_W)] == ALIVE)
+    count++;
+  //Right
+  if (gameArr[CLAMP_C(c + 1) + (r * GAME_W)] == ALIVE)
+    count++;
+  //Up
+  if (gameArr[c + (CLAMP_R(r - 1) * GAME_W)] == ALIVE)
+    count++;
+  //Down
+  if (gameArr[c + (CLAMP_R(r + 1) * GAME_W)] == ALIVE)
+    count++;
+  //UL
+  if(gameArr[CLAMP_C(c-1) + (CLAMP_R(r-1) * GAME_W)] == ALIVE)
+    count++;
+  //UR
+  if(gameArr[CLAMP_C(c+1) + (CLAMP_R(r-1) * GAME_W)] == ALIVE)
+      count++;
+  //DL
+  if(gameArr[CLAMP_C(c-1) + (CLAMP_R(r+1) * GAME_W)] == ALIVE)
+      count++;
+  //DR
+  if(gameArr[CLAMP_C(c+1) + (CLAMP_R(r+1) * GAME_W)] == ALIVE)
+      count++;
+  return (count);
+}
+
+void
 drawCell(int r, int c)
 {
   Cell cell = GET_CELL(r,c);
@@ -151,10 +323,13 @@ drawCell(int r, int c)
     drawImage4(r * CELL_H, c * CELL_W, CELL_W, CELL_H, alive);
   if (cell == DEAD)
     drawRect4(r * CELL_H, c * CELL_W, CELL_W, CELL_H, 0);
-  if (cell == BIRTH)
-    drawImage4(r * CELL_H, c * CELL_W, CELL_W, CELL_H, birth);
-  if (cell == DIEING)
-    drawImage4(r * CELL_H, c * CELL_W, CELL_W, CELL_H, dieing);
+  if (vMode == NEXT)
+    {
+      if (cell == BIRTH)
+        drawImage4(r * CELL_H, c * CELL_W, CELL_W, CELL_H, birth);
+      if (cell == DIEING)
+        drawImage4(r * CELL_H, c * CELL_W, CELL_W, CELL_H, dieing);
+    }
 }
 
 void
--- main.h
+++ main.h
@@ -10,14 +10,22 @@
 #define GAME_W 30
 #define GAME_H 20
 #define GET_CELL(r,c) gameArr[(c) + (r) * GAME_W]
-typedef enum State{TITLE, RESET, NORMAL, NEXT, SIM, SIM_NEXT} State;
-typedef enum Cell{ALIVE, DEAD, DIEING, BIRTH} Cell;
+#define CLAMP_R(r) ((r)<0?GAME_H-1:((r)>=GAME_H?0:(r)))
+#define CLAMP_C(c) ((c)<0?GAME_W-1:((c)>=GAME_W?0:(c)))
+typedef enum State{TITLE, RESET, EDIT, RUN, RUN1} State;
+typedef enum View{NORMAL, NEXT} View;
+typedef enum Grid{CUSTOM, PRESET1, PRESET2} Grid;
+typedef enum Cell{DEAD, ALIVE, DIEING, BIRTH} Cell;
 extern State state;
 int main();
 void titleScreen();
 void moveCrosshairs(int,int,int,int);
 void drawCell(int,int);
-void normal();
+void run();
+void timeStep();
+int neighborCount(int,int);
+void applyVerbs();
+void edit();
 void reset();
 void drawGrid();
 #endif /* MAIN_H_ */
